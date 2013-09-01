69261b1fcefdb4972cace3f010ff94ca707c9343
Lars Kellogg-Stedman <lars@oddbit.com>
2013-08-31 21:09:22 -0400

    added mops module

---
+++ mops.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+
+class app (object):
+    pass
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    return p.parse_args()
+
+def main():
+    opts = parse_args()
+
+if __name__ == '__main__':
+    main()
+
+
