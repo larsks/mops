5afff950ba3126808cb80390be87cae5c8f746db
Lars Kellogg-Stedman <lars@oddbit.com>
2013-08-31 21:07:55 -0400

    removed cherrypy

--- app.py
+++ app.py
@@ -10,12 +10,6 @@ try:
 except IOError:
    pass
 
-def run_cherrypy_server(app, ip, port=8080):
-   from cherrypy import wsgiserver
-   server = wsgiserver.CherryPyWSGIServer(
-               (ip, port), app, server_name='www.cherrypy.example')
-   server.start()
-
 def run_simple_httpd_server(app, ip, port=8080):
    from wsgiref.simple_server import make_server
    make_server(ip, port, app).serve_forever()
@@ -24,6 +18,8 @@ def run_simple_httpd_server(app, ip, port=8080):
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
+
+import mops
 
 #
 #  main():
@@ -34,8 +30,5 @@ if __name__ == '__main__':
    zapp = imp.load_source('application', 'wsgi/application')
 
    print('Starting WSGIServer on %s:%d ... ' % (ip, port))
-   try:
-       run_cherrypy_server(zapp.application, ip, port)
-   except:
-       print("cherrypy probably not installed - using default simple server ...")
-       run_simple_httpd_server(zapp.application, ip, port)
+   run_simple_httpd_server(zapp.application, ip, port)
+
