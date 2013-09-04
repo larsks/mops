cc6d8db9ff41879ca27872a222d8f056bb5b2da1
Lars Kellogg-Stedman <lars@oddbit.com>
2013-09-03 22:18:00 -0400

    removed call to auth_redirect (which no longer exists)

--- mops.py
+++ mops.py
@@ -49,8 +49,7 @@ def setup_request():
     if 'moves_access_token' in request.session:
         log.info('found moves_access_token')
         request.moves_api = moves.movesAPIEndpoint(
-                request.session['moves_access_token'],
-                auth_redirect)
+                request.session['moves_access_token'])
 
 def fetch_template(viewname):
     global pagecache
@@ -72,7 +71,7 @@ def redirect_on_error(url, status_codes=None):
         def wrapper(*args, **kwargs):
             try:
                 return f(*args, **kwargs)
-            except requests.exceptions.HTTPError as detail:
+            except requests.exceptions.HTTPError as as detail:
                 if status_codes is None or \
                         detail.response.status_code in status_codes:
                     redirect(url)
